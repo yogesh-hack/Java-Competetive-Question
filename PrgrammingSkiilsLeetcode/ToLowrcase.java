56015c0b03480b2d869fa603cbb3df4e71fedff8
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-03-11 12:27:00 +0530

    Create

---
+++ PrgrammingSkiilsLeetcode/ToLowrcase.java
@@ -0,0 +1,22 @@
+// problem : 709
+
+// Input: s = "Hello"
+// Output: "hello"
+
+class Solution {
+    public String toLowerCase(String s) {
+       StringBuilder str = new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)>=65 && s.charAt(i)<=90)
+            {
+                str.append((char)(s.charAt(i)+32));
+            }
+            else
+            {
+                str.append(s.charAt(i));
+            }
+        }
+        return str.toString();  
+    }
+}
